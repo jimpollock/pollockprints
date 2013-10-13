87103117b43e9c88fbfa0c2187e16693ac463742
Jim Pollock <jp@pollockprints.com>
2013-10-13 09:36:31 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pollockprints::Application.config.secret_token = '259eacda0533ef0742cc0ccb378a8c5c23d925793effcb473d4fcd7892945ddd1367b52cc2bbaa92ffa118f1d1d9e5b3f72f0e61c6bb2856c626af90be9386de'
