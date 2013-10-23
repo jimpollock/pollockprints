e26f255c804fbb18ba3dd5ae223551bef6513fc1
Jim Pollock <jp@pollockprints.com>
2013-10-23 10:04:32 -0500

    ebean show page

--- app/models/print.rb
+++ app/models/print.rb
@@ -1,3 +1,3 @@
 class Print < ActiveRecord::Base
-  attr_accessible :image, :name, :paper, :press, :process, :year, :size, :ebeans, :paypal, :release_date
+  attr_accessible :image, :name, :paper, :press, :process, :year, :size, :ebeans, :paypal, :release_date, :run_size
 end
